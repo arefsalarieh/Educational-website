3538654a33ded7645f219db1f0bf557484f49232
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-10 00:39:38 +0330

    fix: custom colors tuned

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ export default {
       colors: {
         primary: '#338D81',
         secondary: '#CCA349',
+        bgLogRegFor: '#46a194',
+        bgbtns:"#cca349",
+        bgnews: '#f7f7f7',
+        bgWriterNews:'#6f97b9',
+        bgBorder:'#ffffff',
       },
+      screens: {
+        'xs':'300px',
+        // => @media (min-width: 300px) { ... }
+    
+      }
     },
     fontFamily: {
       irSans: ['IranSans'],
