109bce7a8698a85fc14bbed129669b68a5d309cc
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-10 00:40:25 +0330

    feat: create and design AntdInputSearch comp

---
+++ src/components/NewsArticel/AntdInputSearch.jsx
@@ -0,0 +1,30 @@
+import { AudioOutlined} from '@ant-design/icons';
+import React from 'react';
+import { Input, Space } from 'antd';
+
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      // color: 'bg-bgbtns',
+      // backgroundColor: '#cca349',
+    }}
+  />
+);
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+const AntdInputSearch = () => (
+  <Space direction="vertical">
+    <Search
+      placeholder="عنوان مورد نظر ...."
+      onSearch={onSearch}
+      style={{
+        width: 200,
+        backgroundColor: '#cca349',
+        focus:"#6c4e0c"
+      }}
+    />
+  </Space>
+);
+export {AntdInputSearch};
