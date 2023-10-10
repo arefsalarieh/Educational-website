1c7bbd53cba6674bb3ec6b6be3b619bae8e61927
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-10 20:35:20 +0330

    feat:change NewsArticle

---
+++ src/screens/NewsArticle/NewsArticle.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { AntdInputSearch } from "./AntdInputSearch";
+import { ListNewsCards } from "./ListNewsCards";
+import React from "react";
+import { Button, Dropdown, Space } from "antd";
+
+const NewsArticle = () => {
+  const [articelList, setArticelList] = useState([
+    {
+      title: " 1آموزش جامع طراحی وب",
+      view: "223",
+      nameWriter: "سمیه",
+      description: "این توضیحات یک هست",
+    },
+    {
+      title: " 2آموزش جامع طراحی وب",
+      view: "123",
+      nameWriter: "وحید",
+      description: "این توضیحات دو هست",
+    },
+    {
+      title: " 3آموزش جامع طراحی وب",
+      view: "14",
+      nameWriter: "رضا",
+      description: "این توضیحات سه هست",
+    },
+    {
+      title: " 4آموزش جامع طراحی وب",
+      view: "100",
+      nameWriter: "14020623",
+      description: "این توضیحات 4 هست",
+    },
+    {
+      title: " 2آموزش جامع طراحی وب",
+      view: "95",
+      nameWriter: "عارف",
+      description: ".....   این توضیحات دو هست" ,
+    },
+    {
+      title: " 3آموزش جامع طراحی وب",
+      view: "46",
+      nameWriter: "مزیم",
+      description: "این توضیحات سه هست",
+    },
+    {
+      title: " 4آموزش جامع طراحی وب",
+      view: "178",
+      nameWriter: "زهرا",
+      description: "این توضیحات 4 هست",
+    },
+  ]);
+
+  return (
+    <>
+      {/* Global Container */}
+      <div className="flex w-screen flex-col items-center mx-auto h-screen gap-6">
+        {/* Images */}
+        <div className="w-full mx-auto justify-center ">
+          <img
+            src="/public/assets/img/newsPic-removebg.png"
+            className="mx-auto justify-center "
+            alt="newsPic"
+          />
+        </div>
+        {/* Title and Input Seach  */}
+        <div className="w-full flex flex-col items-center  gap-6">
+          <p className="mx-auto font-bold font-sans text-lg sm:text-2xl md:text-4xl">
+            {" "}
+            اخبار و مقالات{" "}
+          </p>
+          <AntdInputSearch className="flex  justify-items-center mx-auto placeholder:font-sans placeholder:font-light placeholder:text-[10px]]" />
+        </div>
+        {/*Tthe ListOf News and Articels */}
+        <div>
+          <ListNewsCards  articelList={articelList}/>
+        </div>
+        {/*Button More */}
+        <div>
+          <Button className="border border-bgbtns font-sans ">بیشتر</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+export { NewsArticle};
