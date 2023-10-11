240edae713f7202a88953a8b246ef6b92d2bdbb8
aref <aref@example.com>
2023-10-11 11:20:03 +0330

    button and inputEmail

---
+++ src/components/common/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='bg-zgh block text-white p-2.5 px-4'>ارسال</button>
+  )
+}
+
+export default Button
---
+++ src/components/common/inputEmail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './button.jsx'
+
+const InputEmail = () => {
+  return (
+    <div className='border w-96 flex rounded-lg'>
+        <Button/>
+        <input className='block w-full'/>
+    </div>
+  )
+}
+
+export default InputEmail
