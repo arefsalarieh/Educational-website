3afdfe6b9e1f19c6b6ec2ee220f67dd94c67fa84
Vahid <vzadsar27@gmail.com>
2023-10-12 15:37:09 +0330

    "routes updated"

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from "./App/App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./screens/Login/Login.jsx";
-import RouteError from "./screens/Errors/RouteError.jsx";
+import ErrorPage from "./screens/Errors/ErrorPage.jsx";
 import UnauthenticatedApp from "./App/UnauthenticatedApp/UnauthenticatedApp.jsx";
+import ResetPassword from "./screens/ResetPassword/ResetPassword.jsx";
+import ForgotPassword from "./screens/ForgotPassword/ForgotPassword.jsx";
 
 const route = createBrowserRouter([
   {
@@ -15,19 +17,29 @@ const route = createBrowserRouter([
       {
         path: "/",
         element: <UnauthenticatedApp />,
-        errorElement: <RouteError />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/login",
         element: <Login />,
-        errorElement: <RouteError />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/forgotPassword",
+        element: <ForgotPassword />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "resetPassword",
+        element: <ResetPassword />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "*",
-        element: <RouteError />,
+        element: <ErrorPage />,
       },
     ],
-    errorElement: <RouteError />
+    errorElement: <ErrorPage />
   }
 ]);
 
