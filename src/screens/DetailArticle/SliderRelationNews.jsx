07e227bb212eef23e1118d15718979ca574b964b
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-16 15:41:13 +0330

    feat: change SingleRelationNews.jsx

--- src/screens/DetailArticle/SliderRelationNews.jsx
+++ src/screens/DetailArticle/SliderRelationNews.jsx
@@ -1,13 +1,25 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+} from "swiper/modules";
 import "../../../node_modules/swiper/swiper-bundle.min.css";
+import "../../../node_modules/swiper/swiper-bundle.min.js";
 import { SingleArticleDetail } from "./SingleArticleDetail";
 
-const SliderRelationNews = () => {
+import "../../../node_modules/swiper/modules/virtual";
+// import { Virtual } from 'swiper/modules';
+
+// import { ListArticleDetail } from "./ListArticleDetail";
+
+const SliderRelationNews = ({ articelList }) => {
   return (
     <>
       <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Virtual]}
         spaceBetween={50}
         breakpoints={{
           // when window width is >= 640px
@@ -16,19 +28,35 @@ const SliderRelationNews = () => {
           768: {
             slidesPerView: 2,
           },
-
           1024: {
             slidesPerView: 3,
           },
+          1280: {
+            slidesPerView: 4,
+          },
         }}
         slidesPerView={1}
-        navigation
+        navigation={true}
         pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        autoplay={{ delay: 3500, disableOnInteraction: false }}
+        loop={true}
+        // scrollbar={{ draggable: true }}
       >
-        <SwiperSlide>
-          <SingleArticleDetail />
-        </SwiperSlide>
+        <div className=" flex flex-row gap-16  ">
+          {articelList.map((card, index) => {
+            return (
+              <SwiperSlide key={card}>
+                <SingleArticleDetail
+                  key={index}
+                  id={card.id}
+                  title={card.title}
+                  date={card.date}
+                  // pic={card.pic}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </div>
       </Swiper>
     </>
   );
