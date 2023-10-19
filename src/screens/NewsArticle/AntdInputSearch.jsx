ff20cb687d6fa7e974620f6fa6446b3fe04d9f70
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-19 12:23:43 +0330

    feat:change AntdInputSearch

--- src/screens/NewsArticle/AntdInputSearch.jsx
+++ src/screens/NewsArticle/AntdInputSearch.jsx
@@ -20,7 +20,7 @@ const AntdInputSearch = () => (
       placeholder="عنوان مورد نظر ...."
       onSearch={onSearch}
       style={{
-        width: 270,
+        width:290,
         backgroundColor: '#cca349',
         focus:"#6c4e0c"
       }}
