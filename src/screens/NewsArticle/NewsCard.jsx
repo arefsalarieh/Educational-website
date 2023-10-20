ba9659b6c19e1753a04d7c08075b1aa1bbfd75f5
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-20 10:48:44 +0330

    feat:change

--- src/screens/NewsArticle/NewsCard.jsx
+++ src/screens/NewsArticle/NewsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { FaEye } from 'react-icons/fa';
+import { BsBookHalf } from 'react-icons/bs';
 const NewsCard = ({
   id,
   title,
@@ -9,45 +10,40 @@ const NewsCard = ({
 })  => {
   return (
     <>
-      <div className="xl:w-1/6 lg:w-1/5 md:w-1/4 w-full  mx-auto justify-center border border-bg-bgBorder rounded-md p-3">
+      <div className="xl:w-1/6 lg:w-1/5 md:w-1/4  xs:w-full sm:w-1/3  mx-auto justify-center border border-bg-bgBorder rounded-md p-3">
         {/* Image */}
-        <div className="w-full mx-auto justify-center pb-4">
+        <div className="w-full mx-auto justify-center pb-4 cursor-pointer">
           <img src="/public/assets/img/itemNewsPic.jpg" alt="NewsPic" />
         </div>
         {/* Text below the photo */}
-        <div className="px-2">
+        <div className="mx-auto">
           {/* Title */}
           <div>
-            <p className=" text-justify  text-sm  font-semibold font-sans whitespace-nowrap pb-2">
+            <p className=" text-justify  xs:text-sm  font-bold font-sans whitespace-nowrap pb-2 md:text-[11px] lg:text-[12px] sm:text-[12px]">
               {title}
-            {/* آموزش ری اکت جی اس با استاد جدید */}
             </p>
           </div>
           {/* Description */}
-          <div className="mx-auto">
-            <p className=" text-justify text-xs font-sans pb-2 text-ellipsis">
+          <div className="mx-auto ">
+            <p className=" text-justify text-xs font-sans pb-2  truncate">
             {description}
-              {/* لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
-              ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز
-              و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای
-              زیادی در شصت و سه درصد گذشته حال و آینده{" "} */}
             </p>
           </div>
           {/* Writer and View */}
-          <div className="flex justify-between">
+          <div className="flex justify-between ">
             {/*View */}
-            <div>
-              <p className=" text-justify  text-[11px] font-sans font-light">
+            <div className="flex gap-1">
+            <FaEye className="text-bgbtns"/>
+            {/* font-light */}
+              <p className=" text-justify  text-[11px] font-sans  text-bgWriterNews font-bold">
                 {view}
-                {/* 232 */}
               </p>
             </div>
             {/* Writer*/}
-            <div>
+            <div className="flex gap-1">
+              {/* <BsBookHalf className="text-bgWriterNews"/> */}
               <p className=" text-justify  text-xs text-bgWriterNews  font-semibold font-sans">
                 {nameWriter}
-                {/* مریم مهاجر */}
               </p>
             </div>
           </div>
