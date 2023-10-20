c4dbbd5bda1309fe26ce9d0464c6d6d9a43c4aa1
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-20 10:51:13 +0330

    feat:change comp

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,10 @@ import RouteError from "./screens/Errors/RouteError.jsx";
 import UnauthenticatedApp from "./App/UnauthenticatedApp/UnauthenticatedApp.jsx";
 import { NewsArticle } from "./screens/NewsArticle/NewsArticle.jsx";
 import { DetailArticle } from "./screens/DetailArticle/DetailArticle.jsx";
+import { InsertComment } from "./screens/DetailArticle/InsertComment.jsx";
+import { SingleArticleDetail } from "./screens/DetailArticle/SingleArticleDetail.jsx";
+import { Comments } from "./screens/DetailArticle/Comments.jsx";
+import { MenuDetail } from "./screens/DetailArticle/MenuDetail.jsx";
 
 
 const route = createBrowserRouter([
@@ -33,6 +37,21 @@ const route = createBrowserRouter([
       {
         path: "/DetailArticle",
         element: <DetailArticle/>,
+        errorElement: <RouteError />,
+      },
+      {
+        path: "/InsertComment",
+        element: <InsertComment/>,
+        errorElement: <RouteError />,
+      },  
+      {
+        path: "/Comments",
+        element: <Comments/>,
+        errorElement: <RouteError />,
+      }, 
+      {
+        path: "/MenuDetail",
+        element: <MenuDetail/>,
         errorElement: <RouteError />,
       },
       {
