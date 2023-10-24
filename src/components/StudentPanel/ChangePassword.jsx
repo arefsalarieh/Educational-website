922963c35ecf47e7ede18f13ccc9f5fa42260ff4
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-24 10:49:17 +0330

    chsmge

--- src/components/StudentPanel/ChangePassword.jsx
+++ src/components/StudentPanel/ChangePassword.jsx
@@ -7,7 +7,7 @@ const ChangePassword = () => {
     <>
       <Formik>
         <form>
-          <div className="flex flex-col gap-6 md:p-5">
+          <div className="flex flex-col gap-6 md:p-3">
             {/* Top img Side  */}
             <div className=" w-ful flex justify-center">
               <img
@@ -20,15 +20,14 @@ const ChangePassword = () => {
             {/* Below Content Side */}
             <div className=" w-ful flex  flex-col  gap-6 justify-center">
               <h1 className="font-irSans font-bold  text-center text-[20px]  md:text-[26px] ">
-                {" "}
                 تغییر رمز عبور
               </h1>
-              <div className="flex flex-col gap-4  md:gap-8 p-6">
+              <div className="flex flex-col gap-4  md:gap-8 p-6 md:p-3">
                 <ContentChangePass />
                 {/*end Below Content Side */}
                 {/*Boutton */}
-                <div className=" w-full flex justify-center   font-bold text-white mt-2  rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90  hover:shadow-lg border transition    hover:-translate-y-0.5 duration-150 py-1 text-sm  md:text-[16px]  md:py-2  md:w-3/4   md:relative md:right-1/4  lg:w-10/12   lg:right-[120px]  xl:right-[130px]">
-                  <button className="font-irSans">ذخیره تغییرات</button>
+                <div className=" w-full flex justify-center cursor-pointer  font-bold text-white mt-2  md:mt-0 rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90  hover:shadow-lg border transition    hover:-translate-y-0.5 duration-150 py-1 text-sm  md:text-[16px]  md:py-2  md:w-3/4   md:relative md:right-1/4  lg:w-[82.333%]   lg:right-[130px]  xl:w-[86.120%] xl:right-[127px]">
+                  <button className="font-irSans md:py-1">ذخیره تغییرات</button>
                 </div>
                 {/*end Boutton */}
               </div>
--- src/components/StudentPanel/InputPasswordComp.jsx
+++ src/components/StudentPanel/InputPasswordComp.jsx
@@ -6,7 +6,7 @@ const InputPasswordComp = () => {
       <Input.Password
         size="default md:large md:py-2 "
         placeholder="************"
-        className="flex-row-reverse  text-left  w-full  border border-gray-400 rounded-md placeholder:font-irSans placeholder:font-light"
+        className="flex-row-reverse  text-left  w-full  border border-gray-400 rounded-md placeholder:font-irSans placeholder:font-light cursor-pointer "
         dir="ltr"
         iconRender={(visible) =>
           visible ? <EyeTwoTone/> : <EyeInvisibleOutlined />
