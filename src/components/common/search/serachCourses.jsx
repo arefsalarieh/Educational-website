68e4495f7d5ae41e336ef339079c9f5c3c62d784
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-25 14:42:53 +0330

    fsdf

--- src/components/common/search/serachCourses.jsx
+++ src/components/common/search/serachCourses.jsx
@@ -4,7 +4,7 @@ const SearchCourses = ({backgroundColor='bg-zgh' , placeholder='جستجوی خ�
   return (
   <div className={`border mx-auto  flex rounded-lg  overflow-hidden ${height} ${width}`}>
     <input className='block w-full pr-4' placeholder={placeholder}/>    
-    <button className={` block  bg-magnifier bg-40 bg-no-repeat bg-center  rounded-none w-10 md:w-12  text-white p-2.5 px-4  ${backgroundColor}`}></button>
+    <button className={` block  bg-magnifier bg-50 bg-no-repeat bg-center  rounded-none w-10 md:w-12  text-white p-2.5 px-4  ${backgroundColor}`}></button>
   </div>
   )
 }
