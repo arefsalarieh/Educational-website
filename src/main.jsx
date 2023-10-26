b9a3c4d13d0b2ba9145b27fcf8f4810c9263d4f9
Vahid <vzadsar27@gmail.com>
2023-10-26 13:30:42 +0330

    syncing

--- src/main.jsx
+++ src/main.jsx
@@ -2,47 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from "./screens/Login/Login.jsx";
-import RouteError from "./screens/Errors/RouteError.jsx";
-import UnauthenticatedApp from "./App/UnauthenticatedApp/UnauthenticatedApp.jsx";
-import { NewsArticle } from "./screens/NewsArticle/NewsArticle.jsx";
-import { MenuDetail } from "./screens/DetailArticle/MenuDetail.jsx";
-
-
-const route = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <UnauthenticatedApp />,
-        errorElement: <RouteError />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-        errorElement: <RouteError />,
-      },
-      {
-        path: "/NewsArticle",
-        element: <NewsArticle/>,
-        errorElement: <RouteError />,
-      },
-      {
-        path: "/MenuDetail",
-        element: <MenuDetail/>,
-        errorElement: <RouteError />,
-      },
-      {
-        path: "*",
-        element: <RouteError />,
-      }
-    ],
-    errorElement: <RouteError />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
