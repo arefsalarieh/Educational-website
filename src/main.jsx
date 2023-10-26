5ebdac721f51927a26666bd147971734babeb7b4
Vahid <vzadsar27@gmail.com>
2023-10-26 13:27:48 +0330

    syncing

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,6 @@ import Login from "./screens/Login/Login.jsx";
 import RouteError from "./screens/Errors/RouteError.jsx";
 import UnauthenticatedApp from "./App/UnauthenticatedApp/UnauthenticatedApp.jsx";
 import { NewsArticle } from "./screens/NewsArticle/NewsArticle.jsx";
-import { DetailArticle } from "./screens/DetailArticle/DetailArticle.jsx";
-import { InsertComment } from "./screens/DetailArticle/InsertComment.jsx";
-import { SingleArticleDetail } from "./screens/DetailArticle/SingleArticleDetail.jsx";
-import { Comments } from "./screens/DetailArticle/Comments.jsx";
 import { MenuDetail } from "./screens/DetailArticle/MenuDetail.jsx";
 
 
@@ -34,21 +30,6 @@ const route = createBrowserRouter([
         element: <NewsArticle/>,
         errorElement: <RouteError />,
       },
-      {
-        path: "/DetailArticle",
-        element: <DetailArticle/>,
-        errorElement: <RouteError />,
-      },
-      {
-        path: "/InsertComment",
-        element: <InsertComment/>,
-        errorElement: <RouteError />,
-      },  
-      {
-        path: "/Comments",
-        element: <Comments/>,
-        errorElement: <RouteError />,
-      }, 
       {
         path: "/MenuDetail",
         element: <MenuDetail/>,
