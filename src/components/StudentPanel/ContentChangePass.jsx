85d9b97698477cefeb1d878cccd617e50b2c0365
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-10-26 15:03:50 +0330

    feat:change ContentChangePass comp

--- src/components/StudentPanel/ContentChangePass.jsx
+++ src/components/StudentPanel/ContentChangePass.jsx
@@ -3,20 +3,20 @@ import { InputPasswordComp } from "./InputPasswordComp";
 const ContentChangePass = () => {
   return (
     <>
-      <div className="md:flex">
-        <p className="font-irSans mb-2 font-bold md:font-semibold  whitespace-nowrap  w-[135px] text-sm md:text-[16px]  pl-[46px]">
+      <div className="md:flex ">
+        <p className="font-irSans mb-2 md:mb-0 md:mt-[9px] font-bold md:font-semibold  whitespace-nowrap  w-[135px] text-sm md:text-[16px]  pl-[46px]">
           پسورد فعلی
         </p>
         <InputPasswordComp />
       </div>
       <div className=" md:flex">
-        <p className="font-irSans mb-2 font-bold md:font-semibold   whitespace-wrap  md:whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-[44px]">
+        <p className="font-irSans mb-2  md:mb-0 md:mt-[9px]  font-bold md:font-semibold   whitespace-wrap  md:whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-[44px]">
           پسورد جدید
         </p>
         <InputPasswordComp />
       </div>
       <div className="md:flex">
-        <p className="font-irSans mb-2 font-bold md:font-semibold    whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-2">
+        <p className="font-irSans mb-2  md:mb-0 md:mt-[9px]  font-bold md:font-semibold    whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-2">
           تکرار پسورد جدید
         </p>
         <InputPasswordComp />
