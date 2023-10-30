a22e7bc6e1041f2db351fde8597aa216944a68f2
aref <aref@example.com>
2023-10-30 16:42:55 +0330

    NewsArticle animate

--- src/components/NewsArticleComponent/NewsArticleItem.jsx
+++ src/components/NewsArticleComponent/NewsArticleItem.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 const NewsArticleItem = ({title , desc , pic , teacher}) => {
   return (
-    <div className='sm:w-1/2 lg:w-1/3 relative sm:text-right p-4 '>
-    <div className='border-2 p-2 pb-8'>
-        <img src={pic} alt="" />
-        <h2 className='text-lg font-extrabold mt-4'>{title}</h2>
-        <h5 className=' mt-4'>{desc} </h5>
-        <a className='sm:absolute block mt-4 mx-auto sm:mx-0  left-8 bottom-6 text-blue-500'>{teacher} </a>            
-    </div>
+    <motion.div
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{ delay:0.3}}
+    className='sm:w-1/2 lg:w-1/4 relative sm:text-right p-4 '>
+        <div className='border-2 p-2 pb-8'>
+            <img src={pic} alt="" />
+            <h2 className='text-lg font-extrabold mt-4'>{title}</h2>
+            <h5 className=' mt-4'>{desc} </h5>
+            <a className='sm:absolute block mt-4 mx-auto sm:mx-0  left-8 bottom-6 text-blue-500'>{teacher} </a>            
+        </div>
 
-</div>
+    </motion.div>
   )
 }
 
--- src/screens/NewsArticle/NewsArticle.jsx
+++ src/screens/NewsArticle/NewsArticle.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import BigSearch from '../../components/common/BigSearch/BigSearch';
 import NewsArticleComponent from '../../components/NewsArticleComponent/NewsArticleComponent';
+import { motion } from 'framer-motion'
 
 const NewsArticle = () => {
   return (
-    <div className='text-center font-irSans'>
+    <motion.div
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{duration:0.5}}  
+    className='text-center font-irSans'>
 
-    <section>
-        <div className='mx-auto w-2/5 mt-12'><img src="laptop.png" alt="" /></div>
-    </section>
+      <motion.section
+      initial={{opacity:0 , y:-100}}
+      animate={{opacity:1 , y:0}}
+      transition={{duration:0.5}}  
+      >
+          <div className='mx-auto w-2/5 mt-12'><img src="laptop.png" alt="" /></div>
+      </motion.section>
 
-    <h2 className='mb-12 font-extrabold text-xl mt-8'>اخبار و مقالات  </h2>
+      <h2 className='mb-12 font-extrabold text-xl mt-8'>اخبار و مقالات  </h2>
 
-    <BigSearch placeholder='عنوان مورد نظر ...'/>
+      <BigSearch placeholder='عنوان مورد نظر ...'/>
 
-    <NewsArticleComponent/>
+      <NewsArticleComponent/>
 
 
-  </div>
+  </motion.div>
   )
 }
 
