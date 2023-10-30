94ec8d85c1bc10fce1fdc8de68a1fc9069a4459e
aref <aref@example.com>
2023-10-30 10:45:41 +0330

    animate and edit component

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import SingleNewsAndArticle from '../common/SingleNewsAndArticle/SingleNewsAndArticle'
+
+const Article = () => {
+    const [newsList , setNewsList] = useState([
+        { pic:'./news.png' , desc:'م ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله و برای شرایط فعلی تکنولوژی مورد نیاز و کاربرد  ' },
+        { pic:'./news.png' , desc:'م ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله و برای شرایط فعلی تکنولوژی مورد نیاز و کاربرد  ' },
+        { pic:'./news.png' , desc:'م ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله و برای شرایط فعلی تکنولوژی مورد نیاز و کاربرد  ' },
+    ])
+
+  return (
+    <>
+        {newsList.map((item , index)=>{
+            return(
+                <SingleNewsAndArticle key={index} pic={item.pic} desc={item.desc}/>                
+            )
+        })}
+
+    </>
+  )
+}
+
+export default Article
