919e8ddabb74593286d4927537fa44c341a9257a
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-11-17 09:51:58 +0330

    feat:change user.js

--- src/redux/user.js
+++ src/redux/user.js
@@ -5,16 +5,22 @@ const userSlice = createSlice({
     name: 'user',
     initialState: {
         email: "example@example.com",
-        name: "example"
+        name: "example",
+        number:"09111111111"
     },
     reducers: {
         onEmailChange: (state, action) => {
             state.email = action.payload
+        }, 
+        onNumberChange: (state, action) => {
+            state.number = action.payload
         }
     },
 });
 
 export const {onEmailChange} = userSlice.actions; // common way
+
+export const {onNumberChange} = userSlice.actions;
 
 export const useUserSelector = () => useSelector(reducer => reducer.user) // costume hook
 
