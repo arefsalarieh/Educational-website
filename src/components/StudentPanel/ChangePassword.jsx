141f5bdf06a8db509b4336360e7cbf8194f7b7f8
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-11-19 13:24:53 +0330

    FSds

--- src/components/StudentPanel/ChangePassword.jsx
+++ src/components/StudentPanel/ChangePassword.jsx
@@ -1,39 +1,133 @@
 import React, { useEffect, useState } from "react";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { ContentChangePass } from "./ContentChangePass";
+import * as yup from "yup";
+import { InputPasswordComp } from "./InputPasswordComp";
+import toast from "react-hot-toast";
+import http from "../../core/services/interceptor";
+import { useQuery } from "react-query";
+import { getItem } from "../../core/services/common/storage.services";
+
+
 
 const ChangePassword = () => {
+
+ 
+  const onSubmit = async (values) => {
+    const bodyCange = {
+      oldPassword: values.oldPass,
+      newPassword: values.newPass,
+    };
+    console.log(bodyCange);
+    const result = await http.post("/SharePanel/ChangePassword", bodyCange);
+    console.log(result);
+    if (result.success === true) {
+      alert(result.message);
+      toast.success(result.message);
+    } else {
+      toast.error(result.errors);
+    }
+    console.log(result);
+    return result;
+  };
+  
+  const validation = yup.object().shape({
+    oldPass: yup.string().required(" لطفا پسورد فعلی خود را وارد نمایید "),
+    newPass: yup.string().required("لطفا پسورد جدید را وارد کنید"),
+    newConfirm: yup.string().required("لطفا پسورد جدید را وارد کنید"),
+  });
+
   return (
     <>
-      <Formik>
-        <form>
-          <div className="flex flex-col gap-6 md:p-3">
-            {/* Top img Side  */}
-            <div className=" w-ful flex justify-center">
-              <img
-                src="/src/assets/images/changePass.jpg"
-                alt="ChangePassPic"
-                className="w-max rounded-2xl"
-              />
-            </div>
-            {/* end Top img Side  */}
-            {/* Below Content Side */}
-            <div className=" w-ful flex  flex-col  gap-6 justify-center">
-              <h1 className="font-irSans font-bold  text-center text-[20px]  md:text-[26px] ">
-                تغییر رمز عبور
-              </h1>
-              <div className="flex flex-col gap-4  md:gap-8 p-6 md:p-3">
-                <ContentChangePass />
-                {/*end Below Content Side */}
-                {/*Boutton */}
-                <div className=" w-full flex justify-center cursor-pointer  font-bold text-white mt-2  md:mt-0 rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90  hover:shadow-lg border transition    hover:-translate-y-0.5 duration-150 py-1 text-sm  md:text-[16px]    md:w-3/4   md:relative md:right-1/4  lg:w-[82.333%]   lg:right-[130px]  xl:w-[86.120%] xl:right-[127px]">
-                  <button className="font-irSans py-1 md:py-2">ذخیره تغییرات</button>
+      <Formik
+        initialValues={{
+          oldPass: "",
+          newPass: "",
+          newConfirm: "",
+        }}
+        validationSchema={validation}
+        onSubmit={onSubmit}
+      >
+        {({values,handleSubmit, handleChange}) => (
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6 md:p-3">
+              {/* Top img Side  */}
+              <div className=" w-ful flex justify-center">
+                <img
+                  src="/src/assets/images/changePass.jpg"
+                  alt="ChangePassPic"
+                  className="w-max rounded-2xl"
+                />
+              </div>
+              {/* end Top img Side  */}
+              {/* Below Content Side */}
+              <div className=" w-ful flex  flex-col  gap-6 justify-center">
+                <h1 className="font-irSans font-bold  text-center text-[20px]  md:text-[26px] ">
+                  تغییر رمز عبور
+                </h1>
+                <div className="flex flex-col gap-4  md:gap-8 p-6 md:p-3">
+                  <div className="md:flex">
+                    <p className="font-irSans mb-2 font-bold md:font-semibold  whitespace-nowrap  w-[135px] text-sm md:text-[16px]  pl-[46px]">
+                      پسورد فعلی
+                    </p>
+                    <InputPasswordComp
+                      name="oldPass"
+                      value={values.oldPass}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="oldPass"
+                    component={"p"}
+                    className="text-red-600 text-[14px] whitespace-nowrap"
+                  />
+                  <div className=" md:flex">
+                    <p className="font-irSans mb-2 font-bold md:font-semibold   whitespace-wrap  md:whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-[44px]">
+                      پسورد جدید
+                    </p>
+                    <InputPasswordComp
+                      name="newPass"
+                      value={values.newPass}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="newPass"
+                    component={"p"}
+                    className="text-red-600 text-[14px] whitespace-nowrap"
+                  />
+                  <div className="md:flex">
+                    <p className="font-irSans mb-2 font-bold md:font-semibold    whitespace-wrap  md:whitespace-nowrap  w-[135px] text-sm md:text-[16px] pl-2">
+                      تکرار پسورد جدید
+                    </p>
+                    <InputPasswordComp
+                      name="newConfirm"
+                      value={values.newConfirm}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="newConfirm"
+                    component={"p"}
+                    className="text-red-600 text-[14px] whitespace-nowrap"
+                  />
+                  {/*end Below Content Side */}
+                  {/*Boutton */}
+                  <div className=" w-full flex justify-center cursor-pointer  font-bold text-white mt-2  md:mt-0 rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90  hover:shadow-lg border transition    hover:-translate-y-0.5 duration-150 py-1 text-sm  md:text-[16px]    md:w-3/4   md:relative md:right-1/4  lg:w-[82.333%]   lg:right-[130px]  xl:w-[86.120%] xl:right-[127px]">
+                    <button
+                      className="font-irSans py-1 md:py-2"
+                      type="submit"
+                      onSubmit={onSubmit}
+                    >
+                      ذخیره تغییرات
+                    </button>
+                  </div>
+                  {/*end Boutton */}
                 </div>
-                {/*end Boutton */}
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        )}
       </Formik>
     </>
   );
