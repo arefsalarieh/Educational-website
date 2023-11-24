f3dd1c85df98dd9acb3dbe55373780655643a613
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-11-24 17:56:13 +0330

    bbnn

--- src/components/StudentPanel/InputPasswordComp.jsx
+++ src/components/StudentPanel/InputPasswordComp.jsx
@@ -1,17 +1,24 @@
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import { Button, Input, Space } from "antd";
+import { Field } from "formik";
 
-const InputPasswordComp = () => {
+const InputPasswordComp = ({ name }) => {
   return (
-      <Input.Password
-        size="default md:large md:py-2 "
-        placeholder="************"
-        className="flex-row-reverse  text-left  w-full  border border-gray-400 rounded-md placeholder:font-irSans placeholder:font-light cursor-pointer "
-        dir="ltr"
-        iconRender={(visible) =>
-          visible ? <EyeTwoTone/> : <EyeInvisibleOutlined />
-        }
-      />
+    <Field name={name}>
+      {({ field }) => (
+        <Input.Password
+          {...field}
+          size="default md:large md:py-2 "
+          placeholder="************"
+          className="flex-row-reverse  text-left  w-full  border border-gray-400 rounded-md placeholder:font-irSans placeholder:font-light cursor-pointer "
+          dir="ltr"
+          iconRender={(visible) =>
+            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+          }
+        />
+      )}
+    </Field>
   );
 };
-export {InputPasswordComp}
+export { InputPasswordComp };
+
