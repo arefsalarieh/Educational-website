080b7c0b3eb604e45ceb1e25dcab3f64bf1fac34
Vahid <vzadsar27@gmail.com>
2023-11-30 13:19:53 +0330

    syncing

--- src/components/PishForm/PishForm.jsx
+++ src/components/PishForm/PishForm.jsx
@@ -23,14 +23,14 @@ const PishForm = () => {
     {({values , handleSubmit , handleChange})=>(
             <form onSubmit={handleSubmit} className=' shadow-xl mx-auto lg:mr-48 mt-12 pt-6 px-4 sm:px-12 w-8/12 dark:bg-slate-700 rounded-2xl dark:text-stone-200 font-irSans dark:shadow-shadowDarkUp'    labelCol={{span: 8,}}   >
       
-            <label for='d1'>ایمیل شما</label>
+            <label htmlFor='d1'>ایمیل شما</label>
             <Form.Item id='d1' className='w-full  mt-4 rounded-xl' name="email" rules={[{ required: true, message: 'Please input your username!',},]}>
             <Input name='email' value={values.email} onChange={handleChange} 
               className='w-full h-14 text-xl p-4' style={{direction:'ltr'}} placeholder='Maryammohajer@gmail.com'/>
               <ErrorMessage name='email'/>
             </Form.Item>
   
-            <label for='d2'>متن پیام شما</label>
+            <label htmlFor='d2'>متن پیام شما</label>
             <Form.Item id='d2' className='mt-4'>
               <TextArea name='text'  value={values.text} onChange={handleChange} 
               className='h-24 rounded-xl' style={{direction:'ltr' , height:'160px' ,minHeight:'100px'}}/>
--- src/components/common/HeaderSearch/SearchResNull.jsx
+++ src/components/common/HeaderSearch/SearchResNull.jsx
@@ -3,7 +3,7 @@ import { ClipboardDocumentIcon } from "@heroicons/react/24/outline";
 
 function SearchResNull() {
   return (
-    <div className="flex flex-col h-5/6 gap-y-4 items-center justify-center text-gray-400">
+    <div className="flex flex-col h-5/6 gap-y-4 items-center my-5 justify-center text-gray-400">
       <ClipboardDocumentIcon className="w-8 h-8" />
       <p>اطلاعات مرتبط پیدا نشد.</p>
     </div>
